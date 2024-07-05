96f240f5a129b6c1fe9dd08fd621e62d8908cc46
RicardoSimple <98212606+RicardoSimple@users.noreply.github.com>
2024-07-05 14:43:40 +0800

    增加InList函数

---
+++ lists/list.go
@@ -0,0 +1,16 @@
+package lists
+
+// InList
+//
+//	@Description:判断item是否在arr中存在
+//	@param arr 数组/切片
+//	@param item 元素
+//	@return bool
+func InList(arr []interface{}, item interface{}) bool {
+	for _, v := range arr {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
